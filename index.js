e1e94111a10791ea578e9c436df27f85adb45479
Wilver Emanuel Zuñiga Arca <93936766+Wez1toD@users.noreply.github.com>
2023-08-15 23:43:34 -0500

    Upload navbar to mobile

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const sobre_mi_btn = document.getElementById("sobre_mi_btn");
 const proyectos_btn = document.getElementById("proyectos_btn");
 const contacto_btn = document.getElementById("contacto_btn");
-let window_y_size = window.innerHeight;
 
 const sobre_mi_section = document.getElementById("sobre_mi");
 const proyectos_section = document.getElementById("sobre_mi");
 
 let sobre_mi_height = sobre_mi_section.clientHeight;
 let proyectos_height = proyectos_section.clientHeight;
+
+const header_nav = document.querySelector("header nav");
+const header_toggle = document.querySelector(".header_toggle");
 
 sobre_mi_btn.addEventListener("click", () => {
 	window.scrollTo({
@@ -30,4 +32,9 @@ contacto_btn.addEventListener("click", () => {
 		top: sobre_mi_height + proyectos_height,
 		behavior: "smooth",
 	});
+});
+
+header_toggle.addEventListener("click", () => {
+	header_toggle.classList.toggle("active");
+	header_nav.classList.toggle("active");
 });
